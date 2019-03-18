9245db9f6862641743f8b2e4e1c12af97edc3887
Mason Thomas <Kandleseatwicks@gmail.com>
2019-03-18 09:14:37 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -4,7 +4,6 @@ var weather;
 var api = 'https://api.apixu.com/v1/current.json?'
 var apiKey = 'key=e9094bc077f84b29b2d75019191703'
 
-var units = '&units=imperial'
 
 var input;
 
@@ -12,7 +11,11 @@ var city = "~"
 
 
 function preload() {
-  mohave = loadFont('assets/Mohave-Medium.ttf');
+  mohave = loadFont('assets/fonts/Mohave-Medium.ttf');
+  night_clear = loadImage('assets/night weather conditions/Night Clear.png');
+  day_sunny = loadImage('assets/day weather conditions/Day Sunny.png');
+  day_partly_cloudy = loadImage('assets/day weather conditions/Day Partly Cloudy.png');
+  night_partly_cloudy = loadImage('assets/night weather conditions/Night Partly Cloudy.png');
 }
 
 
@@ -37,14 +40,18 @@ function gotData(data) {
 function draw() {
   background(255, 255, 242);
   textFont(mohave)
-  textSize(60);
+
   if (weather) {
     fill(0);
-    textAlign(LEFT,TOP)
-    text(weather.current.temp_f + " ºF",0,130);
-    text(weather.current.condition.text, 0, 70);
+    textAlign(LEFT,BOTTOM)
     textSize(80);
-    text(weather.location.name + ', ' + weather.location.region,0,0);
+    text(weather.location.name + ', ' + weather.location.region,0,80);
+    textSize(60);
+    text(weather.current.condition.text, 0, 150);
+    text(weather.current.temp_f + " ºF",0,220);
+    text(weather.current.wind_mph  + " mph",0,290);
+
+    weatherImage();
   }
 
 }
@@ -52,4 +59,37 @@ function draw() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function weatherImage() {
+  if (weather.current.is_day == "1"){
+    if (weather.current.condition.code == "1000"){
+      image(day_sunny,200,90);
+    }
+    if (weather.current.condition.code == "1003"){
+      image(day_partly_cloudy,290,90);
+    }
+
+
+
+
+
+
+
+
+  } else {
+    if (weather.current.condition.code == "1000"){
+      image(night_clear,200,90);
+  }
+  if (weather.current.condition.code == "1003"){
+    image(night_partly_cloudy,290,90);
+  }
+
+
+
+
+
+
+
+}
 }
