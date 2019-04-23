f1cc242c042adffc387d7b06486f00b5e61debde
Mason Thomas <mason@Masons-Mac-mini.local>
2019-04-22 22:13:41 -0500

    i really dont even know

--- weather.js
+++ weather.js
@@ -1,14 +1,18 @@
-
 var weather;
+var darkData;
 
 var api = 'https://api.apixu.com/v1/current.json?'
 var apiKey = 'key=e9094bc077f84b29b2d75019191703'
-
+var forecastApi = 'https://api.apixu.com/v1/forecast.json?'
+var darkApi = ' https://api.darksky.net/forecast/'
+var darkApiKey = 'b4b24fa0c2ca3e613ecb4b6896bf3abe/'
 
 var input;
 
 var city = "~"
 
+var lat;
+var lon;
 
 function preload() {
   mohave = loadFont('assets/fonts/Mohave-Medium.ttf');
@@ -24,7 +28,7 @@ function preload() {
 
 
 function setup() {
-  createCanvas(windowWidth,windowHeight,);
+  createCanvas(windowWidth, windowHeight, );
 
   var button = select('#submit');
   button.mousePressed(weatherAsk);
@@ -34,12 +38,22 @@ function setup() {
 
 function weatherAsk() {
   var url = api + apiKey + '&q=' + input.value()
+  var forecastUrl = forecastApi + apiKey + '&q=' + input.value()
   loadJSON(url, gotData);
+
+  var darkUrl = darkApi + darkApiKey + lat + ',' + lon
+  loadJSON(darkUrl, darkGotData);
 }
 
 function gotData(data) {
   weather = data
+  lat = weather.location.lat
+  lon = weather.location.lon
 }
+function darkGotData(data) {
+  darkData = data
+}
+
 
 function draw() {
   background(255, 255, 242);
@@ -47,16 +61,18 @@ function draw() {
 
   if (weather) {
     fill(0);
-    textAlign(LEFT,BOTTOM)
+    textAlign(LEFT, BOTTOM)
     textSize(80);
-    text(weather.location.name + ', ' + weather.location.region,0,80);
+    text(weather.location.name + ', ' + weather.location.region, 0, 80);
+    textSize(70);
+    text('Current:', 0, 140)
     textSize(60);
-    text(weather.current.condition.text, 0, 150);
-    text(weather.current.temp_f + " ºF",0,220);
-    text(weather.current.wind_mph  + " mph",0,290);
-
-
+    text(weather.current.condition.text, 0, 190);
+    text(weather.current.temp_f + " ºF", 0, 250);
+    //text(darkData.timezone,100,100);
+    console.log(darkData);
     weatherImage();
+    currentParagraph();
   }
 
 }
@@ -67,45 +83,49 @@ function windowResized() {
 }
 
 function weatherImage() {
-  if (weather.current.is_day == "1"){
-    if (weather.current.condition.code == "1000"){
-      image(day_sunny,200,90);
+  if (weather.current.is_day == "1") {
+    if (weather.current.condition.code == "1000") {
+      image(day_sunny, 800, 130);
     }
-    if (weather.current.condition.code == "1003"){
-      image(day_partly_cloudy,290,90);
+    if (weather.current.condition.code == "1003") {
+      image(day_partly_cloudy, 800, 130);
     }
-    if (weather.current.condition.code == "1030"){
-      image(day_mist,290,90);
+    if (weather.current.condition.code == "1030") {
+      image(day_mist, 800, 130);
     }
-    if (weather.current.condition.code == "1063"){
-      image(day_patchy_rain,290,90);
+    if (weather.current.condition.code == "1063") {
+      image(day_patchy_rain, 800, 130);
     }
-
-
-
-
-
-
-
-
   } else {
-    if (weather.current.condition.code == "1000"){
-      image(night_clear,200,90);
+    if (weather.current.condition.code == "1000") {
+      image(night_clear, 800, 130);
+    }
+    if (weather.current.condition.code == "1003") {
+      image(night_partly_cloudy, 800, 130);
+    }
+    if (weather.current.condition.code == "1030") {
+      image(night_mist, 800, 130);
+    }
+    if (weather.current.condition.code == "1063") {
+      image(night_patchy_rain, 800, 130);
+    }
   }
-  if (weather.current.condition.code == "1003"){
-    image(night_partly_cloudy,290,90);
-  }
-  if (weather.current.condition.code == "1030"){
-    image(night_mist,290,90);
-  }
-  if (weather.current.condition.code == "1063"){
-    image(night_patchy_rain,290,90);
-  }
+}
 
 
-
-
-
+function currentParagraph() {
+  textSize(30);
+  textAlign(LEFT, TOP)
+  text("Right now, its " + weather.current.condition.text + ', ', 340, 130);
+  text(weather.current.temp_f + ' ºF, in ' + weather.location.name + '.', 340, 160);
+  text('Currently, it feels like ' + weather.current.feelslike_f + " ºF.", 340, 190);
+  text('There are ' + weather.current.wind_mph + " mph winds blowing " + weather.current.wind_dir + '.', 340, 220);
 
 }
+
+function forecastParagraph() {
+  textSize(30);
+  textAlign(LEFT,TOP)
+  text()
+
 }
